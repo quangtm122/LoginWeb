d75c70e29cb24b8f5b4b4e538af61f636382ec2a
quangtm122 <quangtranht22@gmail.com>
2022-01-07 08:40:01 +0700

    Home

--- Web_MVC/Controllers/HomeController.cs
+++ Web_MVC/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web_MVC.Models;
 
 namespace Web_MVC.Controllers
 {
@@ -10,7 +11,24 @@ namespace Web_MVC.Controllers
     {
         public ActionResult Index()
         {
+            /*QuanLyNLDCEntities1 context = new QuanLyNLDCEntities1();
+
+            var query = context.ThongTins
+                   .GroupBy(p => p.ten)
+                   .Select(g => new { name = g.Key, count = g.Select(w => w.hienTai) }).ToList();
+            return Json(query, JsonRequestBehavior.AllowGet);*/
             return View();
+        }
+
+        public ActionResult GetData()
+        {
+            QuanLyNLDCEntities1 context = new QuanLyNLDCEntities1();
+
+            var query = context.ThongTins
+                    
+                   .GroupBy(p => p.time)
+                   .Select(g => new { time = g.Key.ToString(), value = g.Select(w => w.hienTai), name = g.Select(h=>h.ten.ToString())}).ToList();    
+            return Json(query, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult About()
